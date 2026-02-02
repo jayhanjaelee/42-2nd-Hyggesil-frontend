eff2fbc6f7a8aab303bbbd9f1a65408304d40bbd
Hanjae Lee <jayhanjaelee@gmail.com>
2026-02-02 21:09:19 +0900

    Change server address.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const BASE_URL = 'http://127.0.0.1';
 
-const BASE_URL_S = 'http://127.0.0.1:8000'; // server
-const BASE_URL_H = 'http://127.0.0.1:4000';
+const BASE_URL_S = 'http://127.0.0.1:7100'; // server
+const BASE_URL_H = 'http://127.0.0.1:7100';
 
 export const API = {
   LOGIN: `${BASE_URL_S}/users/user/login`,
